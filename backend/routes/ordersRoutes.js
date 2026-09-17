18052cc3933d4e04ea012654ae8a77bb6433aef4
Anup Kumar <anupjha842@gmail.com>
2026-09-17 16:56:31 +0530

    Update ordersRoutes.js

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const {protect} = require('../middleware/authMiddleware');
 const {admin} = require('../middleware/adminMiddleware');
 
-const {createOrder, getOrders, updateOrderStatus, myOrders} = require('../controllers/orderController');
+const {addOrderItems, getOrders, updateOrderStatus, myOrders} = require('../controllers/orderController');
 
 const router = express.Router();
 
-router.route('/').post(protect,createOrder).get(protect ,admin, getOrders);
+router.route('/').post(protect,addOrderItems).get(protect ,admin, getOrders);
 router.route('/myorders').get(protect, myOrders);
 router.route('/:id/status').put(protect,admin,updateOrderStatus);
 
